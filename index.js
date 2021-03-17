6c47d02abdd56ff163d21de79a5e65c93486d093
climaco1 <climacot@unicauca.edu.co>
2021-03-17 02:52:10 -0500

    heroku

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { response } = require('express')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const logger = require('./loggerMiddleware')
 
 app.use(cors())
 app.use(express.json())
+
 app.use(logger)
 
 let notes = [
@@ -72,7 +73,8 @@ app.use((request, responde) => {
     error: 'Not Found'
   })
 })
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
