ff1545bc005903d3190fe33c9c9adff407a01320
climaco1 <climacot@unicauca.edu.co>
2021-03-17 08:14:11 -0500

    insultando

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ let notes = [
 ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>helo world</h1>')
+  response.send('<h1>hello world putos gonorreas ome gonorrea xdxdxdx :v</h1>')
 })
 
 app.delete('/api/notes/:id', (request, response) => {
